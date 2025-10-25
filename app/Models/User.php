b03ecfc2b9f413692bd32d7f75d19b6f7a16efaa
Emad Isavi <emadesavi@gmail.com>
2025-10-25 19:32:41 +0330

    Modified: add some fields to $fillable

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,14 +3,16 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasApiTokens, SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
@@ -21,7 +23,12 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'mobile',
+        'slug',
+        'city_id',
     ];
+
+    protected $guarded = ['is_admin'];
 
     /**
      * The attributes that should be hidden for serialization.
