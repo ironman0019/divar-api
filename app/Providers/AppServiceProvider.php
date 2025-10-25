d81d5062eafc62a937bdf2332944fa04e8680fbe
Emad Isavi <emadesavi@gmail.com>
2025-10-25 19:31:53 +0330

    Add rate limit by mobile number in project for otp routes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use Illuminate\Http\Request;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Support\Facades\RateLimiter;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +22,18 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        RateLimiter::for('otp', function (Request $request) {
+            $mobile = $request->input('mobile');
+
+            if ($mobile) {
+                // Limit by mobile
+                return Limit::perMinutes(1, 3)->by($mobile);
+            }
+
+            // Fallback: limit by IP
+            return Limit::perMinutes(1, 3)->by($request->ip());
+        });
+
+        // Paginator::useBootstrap();
     }
 }
