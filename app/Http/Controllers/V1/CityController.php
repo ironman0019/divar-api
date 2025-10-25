61bffc69bac0b318e2a4359d2ba64ed7142a8b96
Emad Isavi <emadesavi@gmail.com>
2025-10-25 19:34:30 +0330

    Create city controller and resource

---
+++ app/Http/Controllers/V1/CityController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\V1\CityResource;
+use App\Models\City;
+use App\Traits\HttpResponse;
+use Illuminate\Http\Request;
+
+class CityController extends Controller
+{
+    use HttpResponse;
+
+    public function index()
+    {
+        $cities = City::all();
+        return $this->success(
+            CityResource::collection($cities),
+            'Cities retrieved successfully'
+        );
+    }
+}
---
+++ app/Http/Resources/V1/CityResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources\V1;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CityResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'status' => [
+                'value' => $this->status,
+                'label' => $this->status ? 'فعال' : 'غیرفعال'
+            ],
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at
+        ];
+    }
+}
