b06d064d2363edf6fc41944b1007b4918127fbc4
Emad Isavi <emadesavi@gmail.com>
2025-10-25 19:32:12 +0330

    Create otp model

---
+++ app/Models/User/Otp.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models\User;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Otp extends Model
+{
+    use SoftDeletes;
+
+    protected $guarded = ['id'];
+
+    protected $fillable = [
+        'user_id',
+        'mobile',
+        'token',
+        'otp_code',
+        'used',
+        'attempts',
+        'expires_at',
+        'form_fields'
+    ];
+
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+}
