af8cb0be346e52d811684afd4c5ec2866792c54e
Emad Isavi <emadesavi@gmail.com>
2025-10-28 18:05:10 +0330

    Modified: make the user_id nullable

--- database/migrations/2024_09_15_213829_create_otps_table.php
+++ database/migrations/2024_09_15_213829_create_otps_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('otps', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained('users')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreignId('user_id')->nullable()->constrained('users')->onDelete('cascade')->onUpdate('cascade');
             $table->string('mobile', 11)->nullable();
             $table->json('form_fields')->nullable();
             $table->string('token');
