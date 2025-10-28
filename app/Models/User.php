08b546a5cffd82a0162bfeb6138717069c7172ab
Emad Isavi <emadesavi@gmail.com>
2025-10-28 17:52:50 +0330

    Modified: removed softdeletes trait and fix the fillable var

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,14 +5,13 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable, HasApiTokens, SoftDeletes;
+    use HasFactory, Notifiable, HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
@@ -24,8 +23,8 @@ class User extends Authenticatable
         'email',
         'password',
         'mobile',
-        'slug',
         'city_id',
+        'is_active',
     ];
 
     protected $guarded = ['is_admin'];
