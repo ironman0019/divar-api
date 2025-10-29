9cc9de10c811cbf368c6950ed7f10469b753deeb
Emad Isavi <emadesavi@gmail.com>
2025-10-30 01:59:36 +0330

    Add web auth routes

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\Auth\AuthController;
 use App\Http\Controllers\Admin\AdminDashboardController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('login', [AuthController::class, 'login'])->middleware('guest')->name('login');
+Route::post('login/auth', [AuthController::class, 'auth'])->middleware('guest')->name('login.auth');
+Route::get('logout', [AuthController::class, 'logout'])->middleware('auth')->name('logout');
 
 
-Route::prefix('admin')->middleware([''])->name('admin.')->group(function() {
+Route::prefix('admin')->middleware(['auth', 'admin'])->name('admin.')->group(function() {
 
     Route::get('/', [AdminDashboardController::class, 'index'])->name('dashboard');
 
