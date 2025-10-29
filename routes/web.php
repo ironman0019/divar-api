b91284df8d69cc8b607c91a898837faa4a345016
Emad Isavi <emadesavi@gmail.com>
2025-10-30 01:35:18 +0330

    Add admin panel index page web route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\AdminDashboardController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::prefix('admin')->middleware([''])->name('admin.')->group(function() {
+
+    Route::get('/', [AdminDashboardController::class, 'index'])->name('dashboard');
+
+
 });
