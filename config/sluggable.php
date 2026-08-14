b25b4049910d9b0dddbe02c9bc0bf4cc5851242e
Emad Isavi <emadesavi@gmail.com>
2026-08-14 20:00:27 +0330

    Add Slugger class for generating Unicode-preserving slugs and update configuration to use it

--- config/sluggable.php
+++ config/sluggable.php
@@ -47,83 +47,13 @@ return [
      * If left to "null", then use the cocur/slugify package to generate the slug
      * (with the separator defined below).
      *
-     * Set this to a closure that accepts two parameters (string and separator)
-     * to define a custom slugger.  e.g.:
+     * Set this to a callable class method so config caching works with
+     * `php artisan optimize` / `config:cache` (Closures are not serializable).
      *
-     *    'method' => function( $string, $sep ) {
-     *       return preg_replace('/[^a-z]+/i', $sep, $string);
-     *    },
-     *
-     * Otherwise, this will be treated as a callable to be used.  e.g.:
-     *
-     *    'method' => array('Str','slug'),
+     *    'method' => [App\Support\Slugger::class, 'make'],
      */
 
-        // This have problem when run php artisan optimize
-    'method' => function ($string, $separator = '-') {
-        $_transliteration = array(
-            '/ä|æ|ǽ/' => 'ae',
-            '/ö|œ/' => 'oe',
-            '/ü/' => 'ue',
-            '/Ä/' => 'Ae',
-            '/Ü/' => 'Ue',
-            '/Ö/' => 'Oe',
-            '/À|Á|Â|Ã|Å|Ǻ|Ā|Ă|Ą|Ǎ/' => 'A',
-            '/à|á|â|ã|å|ǻ|ā|ă|ą|ǎ|ª/' => 'a',
-            '/Ç|Ć|Ĉ|Ċ|Č/' => 'C',
-            '/ç|ć|ĉ|ċ|č/' => 'c',
-            '/Ð|Ď|Đ/' => 'D',
-            '/ð|ď|đ/' => 'd',
-            '/È|É|Ê|Ë|Ē|Ĕ|Ė|Ę|Ě/' => 'E',
-            '/è|é|ê|ë|ē|ĕ|ė|ę|ě/' => 'e',
-            '/Ĝ|Ğ|Ġ|Ģ/' => 'G',
-            '/ĝ|ğ|ġ|ģ/' => 'g',
-            '/Ĥ|Ħ/' => 'H',
-            '/ĥ|ħ/' => 'h',
-            '/Ì|Í|Î|Ï|Ĩ|Ī|Ĭ|Ǐ|Į|İ/' => 'I',
-            '/ì|í|î|ï|ĩ|ī|ĭ|ǐ|į|ı/' => 'i',
-            '/Ĵ/' => 'J',
-            '/ĵ/' => 'j',
-            '/Ķ/' => 'K',
-            '/ķ/' => 'k',
-            '/Ĺ|Ļ|Ľ|Ŀ|Ł/' => 'L',
-            '/ĺ|ļ|ľ|ŀ|ł/' => 'l',
-            '/Ñ|Ń|Ņ|Ň/' => 'N',
-            '/ñ|ń|ņ|ň|ŉ/' => 'n',
-            '/Ò|Ó|Ô|Õ|Ō|Ŏ|Ǒ|Ő|Ơ|Ø|Ǿ/' => 'O',
-            '/ò|ó|ô|õ|ō|ŏ|ǒ|ő|ơ|ø|ǿ|º/' => 'o',
-            '/Ŕ|Ŗ|Ř/' => 'R',
-            '/ŕ|ŗ|ř/' => 'r',
-            '/Ś|Ŝ|Ş|Ș|Š/' => 'S',
-            '/ś|ŝ|ş|ș|š|ſ/' => 's',
-            '/Ţ|Ț|Ť|Ŧ/' => 'T',
-            '/ţ|ț|ť|ŧ/' => 't',
-            '/Ù|Ú|Û|Ũ|Ū|Ŭ|Ů|Ű|Ų|Ư|Ǔ|Ǖ|Ǘ|Ǚ|Ǜ/' => 'U',
-            '/ù|ú|û|ũ|ū|ŭ|ů|ű|ų|ư|ǔ|ǖ|ǘ|ǚ|ǜ/' => 'u',
-            '/Ý|Ÿ|Ŷ/' => 'Y',
-            '/ý|ÿ|ŷ/' => 'y',
-            '/Ŵ/' => 'W',
-            '/ŵ/' => 'w',
-            '/Ź|Ż|Ž/' => 'Z',
-            '/ź|ż|ž/' => 'z',
-            '/Æ|Ǽ/' => 'AE',
-            '/ß/' => 'ss',
-            '/Ĳ/' => 'IJ',
-            '/ĳ/' => 'ij',
-            '/Œ/' => 'OE',
-            '/ƒ/' => 'f'
-        );
-
-        $quotedReplacement = preg_quote($separator, '/');
-        $merge = array(
-            '/[^\s\p{Zs}\p{Ll}\p{Lm}\p{Lo}\p{Lt}\p{Lu}\p{Nd}]/mu' => ' ',
-            '/[\s\p{Zs}]+/mu' => $separator,
-            sprintf('/^[%s]+|[%s]+$/', $quotedReplacement, $quotedReplacement) => '',
-        );
-        $map = $_transliteration + $merge;
-        unset($_transliteration);
-        return preg_replace(array_keys($map), array_values($map), $string);
-    },
+    'method' => [\App\Support\Slugger::class, 'make'],
 
     /**
      * Separator to use when generating slugs.  Defaults to a hyphen.
